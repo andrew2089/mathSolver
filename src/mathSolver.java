5acd92f41156a5e131a38f9bee95dfed6ca7d768
andrew2089 <aglushenok9@gmail.com>
2020-10-15 12:05:06 +0300

    Math solver 1 lesson

---
+++ src/mathSolver.java
@@ -0,0 +1,14 @@
+public class mathSolver {
+    public static void main(String[] args) {
+        int a = 74;
+        int b = 36;
+        int sum = a + b;
+        System.out.println("summa slozenija " + sum);
+
+        int t = 50;
+        int y = 3;
+        int result = t / y;
+        System.out.println("rezultat delenija " + result);
+    }
+    }
+
