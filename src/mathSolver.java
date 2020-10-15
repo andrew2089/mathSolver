6561d7eddb7817881b701d9190785f3672cb4985
andrew2089 <aglushenok9@gmail.com>
2020-10-15 12:06:04 +0300

    upadate !

--- src/mathSolver.java
+++ src/mathSolver.java
@@ -3,12 +3,12 @@ public class mathSolver {
         int a = 74;
         int b = 36;
         int sum = a + b;
-        System.out.println("summa slozenija " + sum);
+        System.out.println("summa slozenija! " + sum);
 
         int t = 50;
         int y = 3;
         int result = t / y;
-        System.out.println("rezultat delenija " + result);
+        System.out.println("rezultat delenija! " + result);
     }
     }
 
